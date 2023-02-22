4ecc9c78c0c9e8026755907b140f0406d9e2c6b7
MrJeleika <den932005@gmail.com>
2023-02-22 21:08:36 +0200

    fix

--- controllers/DayLessonController.js
+++ controllers/DayLessonController.js
@@ -67,24 +67,19 @@ export const changeDayLesson = async (req, res) => {
 export const deleteDayLesson = async (req, res) => {
   const { _id, lessonId, date } = req.body
   try {
-    let lesson = await Schedule.findOne(
-      {
-        _id,
-      },
-      {
-        exceptions: { _id: lessonId },
-      }
-    )
+    let schedule = await Schedule.findOne({ _id })
+
+    let lesson = schedule.exceptions.find((ex) => ex._id.equals(lessonId))
+
     if (!lesson) {
       // if not found in week Schedule search in exceptions
-      let lesson = await Lesson.findOne({ _id: lessonId })
+      lesson = await Lesson.findOne({ _id: lessonId })
+
       if (!lesson)
         res
           .status(404)
           .res.json({ message: "Couldn't find lesson with such id" })
-      return lesson
     }
-
     lesson.time = undefined
     lesson.date = date
 
